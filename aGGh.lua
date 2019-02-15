4ab5109093593d346fe7160ce1519175f2ff48f4
BellinRattin <bellinrattin@gmail.com>
2019-02-15 12:25:41 +0100

    add stats table
    added statistics table under the options

--- aGGh.lua
+++ aGGh.lua
@@ -22,7 +22,8 @@ local nameTable = {
 	["CHAT_MSG_CHANNEL"] = "channels",
 }
 
-local settingsDefaults = {
+local aGGDBDefaults = {
+	["version"] = "0.9.0",
 	["channels"] = {
 		["CHAT_MSG_GUILD"] = {
 			["gg"] = true,
@@ -52,15 +53,48 @@ local settingsDefaults = {
 			["gg"] = true,
 			["gz"] = true,
 		},
+	},
+	["stats"] = {
+		["total"] = 0,
+		["badperson"] = {}
 	}
 }
 
 local settings = {}
 
+local badpeople = {}
+local frame_bad = {}
+local function UpdateBadPeople()
+	wipe(badpeople)
+	for k,v in pairs(settings["stats"]["badperson"]) do table.insert(badpeople, {k,v}) end
+	table.sort(badpeople, function(a,b) return a[2]>b[2] end)
+		
+	for i = 1,min(#badpeople,5) do
+		frame_bad[i]:SetText(badpeople[i][1].." "..badpeople[i][2])
+	end
+	frame_bad["all"]:SetText("Total: "..settings["stats"]["total"])
+end
+
+local function InitDB()
+	settings = aGGhDB or aGGDBDefaults
+	local vers = settings["version"]
+	local curr = GetAddOnMetadata(addonName, "Version")
+	if not vers then
+		settings["version"] = curr
+		settings["stats"] = {["total"]=0, ["badperson"] = {}}
+	end
+	if not(vers == curr) then
+		-- if vers == "" then end
+		settings["version"]=curr
+	end
+	aGGhDB = settings
+end
+
 function f:ADDON_LOADED(name)
 	if name == addonName then
-		settings = aGGhDB or settingsDefaults
-		aGGhDB = settings
+		--settings = aGGhDB or DBDefaults
+		--aGGhDB = settings
+		InitDB()
 		self:CreateOptionPanel()
 	end
 end
@@ -73,19 +107,26 @@ local patterns = {
 
 local function PatternFilter(self, event, msg, author,...)
 	local set = settings["channels"][tostring(event)]
+	local bol 
 	if not set["gg"] then
 		if not set["gz"] then
-			return false
+			bol = false
 		else
-			return stringFind(msg, patterns.gz)
+			bol = stringFind(msg, patterns.gz)
 		end
 	else
 		if not set["gz"] then
-			return stringFind(msg, patterns.gg)
+			bol = stringFind(msg, patterns.gg)
 		else
-			return stringFind(msg, patterns.mix)
+			bol = stringFind(msg, patterns.mix)
 		end
 	end
+	if bol then
+		settings["stats"]["total"] = settings["stats"]["total"] +1
+			settings["stats"]["badperson"][author] = (settings["stats"]["badperson"][author] or 0) +1
+		UpdateBadPeople()
+	end
+	return bol
 end
 
 f:RegisterEvent("ADDON_LOADED")
@@ -164,5 +205,51 @@ function f:CreateOptionPanel()
 			end
 		end
 	end
+
+	local header = CreateFrame("Frame", "statsHeader", optionPanel)
+
+	header:SetPoint("TOPLEFT", optionPanel, "TOPLEFT", 0, -250)
+	header:SetPoint("TOPRIGHT", optionPanel, "TOPRIGHT", 0, -250)
+
+	header:SetHeight(18)
+	header.label = header:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
+	header.label:SetPoint("TOP")
+	header.label:SetPoint("BOTTOM")
+	header.label:SetJustifyH("CENTER")
+	header.label:SetText("STATISTICS")
+
+	header.left = header:CreateTexture(nil, "BACKGROUND")
+	header.left:SetHeight(8)
+	header.left:SetPoint("LEFT", 10, 0)
+	header.left:SetPoint("RIGHT", header.label, "LEFT", -5, 0)
+	header.left:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border")
+	header.left:SetTexCoord(0.81, 0.94, 0.5, 1)
+	header.right = header:CreateTexture(nil, "BACKGROUND")
+	header.right:SetHeight(8)
+	header.right:SetPoint("RIGHT", -10, 0)
+	header.right:SetPoint("LEFT", header.label, "RIGHT", 5, 0)
+	header.right:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border")
+	header.right:SetTexCoord(0.81, 0.94, 0.5, 1)
+	
+	local all_label = header:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
+	frame_bad["all"] = all_label
+	all_label:SetPoint("TOP", header, "BOTTOM", 0, -10)
+	all_label:SetJustifyH("CENTER")
+	--all_label:SetText("Total: "..settings["stats"]["total"])
+
+	--local a={}
+	for i=1,5 do
+		local label = header:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
+		frame_bad[i]=label
+		if i == 1 then
+			label:SetPoint("TOP", all_label, "BOTTOM", 0, -10)
+		else
+			label:SetPoint("TOP", frame_bad[i-1], "BOTTOM", 0, -10)
+		end
+		label:SetJustifyH("CENTER")
+		--label:SetText(badpeople[i][1].." "..badpeople[i][2])
+	end
+	UpdateBadPeople()
+
 	InterfaceOptions_AddCategory(optionPanel)
 end
